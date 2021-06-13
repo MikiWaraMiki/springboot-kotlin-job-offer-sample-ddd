3de0c90fdb82ef7b98edb469fe0d5d95d33cbfac
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-06-13 16:01:49 +0900

    add front用のnuxt project

---
+++ front/job-offer-front/nuxt.config.ts
@@ -0,0 +1,47 @@
+import { NuxtConfig } from '@nuxt/types'
+
+const config: NuxtConfig = {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'job-offer-front',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+
+
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/tailwindcss'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
+
+export default config
