ae916092c90aed5ca320be03b9758ee05457821d
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-06-20 15:53:34 +0900

    add roleの等価判定を追加

--- src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/auth/domain/model/role/Role.kt
+++ src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/auth/domain/model/role/Role.kt
@@ -3,7 +3,7 @@ package com.tokorogadokkoi.kotlin.joboffer.auth.domain.model.role
 /**
  * ロールクラス
  */
-class Role(_name: String) {
+class Role(private val id: Int, _name: String) {
     private val name: String
 
     init {
@@ -12,6 +12,38 @@ class Role(_name: String) {
         name = _name
     }
 
+    /**
+     * 等価性判定
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        if (javaClass != other?.javaClass) return false
+
+        other as Role
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    /**
+     * ハッシュコード計算
+     */
+    override fun hashCode(): Int {
+        return 31 * name.hashCode() + id
+    }
+
+    /**
+     * 文字列
+     */
+    override fun toString(): String {
+        return name
+    }
+
+    /**
+     * ロール名のバリデーション
+     */
     private fun validateName(name: String) {
         if (name == "") throw IllegalArgumentException("ロール名の入力は必須です")
 
