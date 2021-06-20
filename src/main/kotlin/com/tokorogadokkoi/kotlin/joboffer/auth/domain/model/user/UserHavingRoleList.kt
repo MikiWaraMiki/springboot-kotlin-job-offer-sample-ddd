f5f0439722369ca268eb0e9925dc125fe20355e8
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-06-20 16:04:28 +0900

    add ユーザ保持ロールのクラス作成

---
+++ src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/auth/domain/model/user/UserHavingRoleList.kt
@@ -0,0 +1,29 @@
+package com.tokorogadokkoi.kotlin.joboffer.auth.domain.model.user
+
+import com.tokorogadokkoi.kotlin.joboffer.auth.domain.model.role.Role
+
+/**
+ * ユーザが保持するロール
+ */
+class UserHavingRoleList {
+    val roleList: MutableList<Role> = mutableListOf()
+
+    /**
+     * ロールを追加する
+     * @param role 追加するロール
+     */
+    fun addRole(role: Role) {
+        roleList.add(role)
+    }
+
+    /**
+     * 引数で与えられたロールを保持しているか判定を行う
+     * @param role 保持しているか判定を行うロール
+     */
+    fun hasRole(targetRole: Role): Boolean {
+        val result = roleList.firstOrNull { role -> role == targetRole
+        } ?: return false
+
+        return true
+    }
+}
