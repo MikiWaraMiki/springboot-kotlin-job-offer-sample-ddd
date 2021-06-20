b86f83c35ccc047c36d31aa1cc8168df4d33f0c4
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-06-20 11:33:07 +0900

    fix DB設計

--- src/main/resources/db/migration/V1.0.0__create_user_table.sql
+++ src/main/resources/db/migration/V1.0.0__create_user_table.sql
@@ -1,8 +1,10 @@
 -- create users
 CREATE TABLE IF NOT EXISTS users (
     uuid BINARY(16) NOT NULL,
+    email VARCHAR(256) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
     updated_at TIMESTAMP NOT NULL DEFAULT  CURRENT_TIMESTAMP ,
     PRIMARY KEY (uuid)
 ) ENGINE= InnoDB;
 ALTER TABLE users ADD INDEX idx_users_created_at(created_at);
+ALTER TABLE users ADD UNIQUE uniq_users_email(email);
