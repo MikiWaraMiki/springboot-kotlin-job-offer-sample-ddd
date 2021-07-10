4e106250ceb06e5aaf34a346c1365444b494b24a
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-07-10 10:36:06 +0900

    add ユーザ登録時のリクエストとレスポンス

---
+++ src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/auth/presentation/user/UserRegistrationRequest.kt
@@ -0,0 +1,5 @@
+package com.tokorogadokkoi.kotlin.joboffer.auth.presentation.user
+
+data class UserRegistrationRequest(
+    val email: String
+)
---
+++ src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/auth/presentation/user/UserRegistrationResponse.kt
@@ -0,0 +1,6 @@
+package com.tokorogadokkoi.kotlin.joboffer.auth.presentation.user
+
+data class UserRegistrationResponse(
+    val uuid: String,
+    val email: String
+)
