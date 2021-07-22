f34f82b022f4c68bb5fb393021c9b34dbac351e1
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-07-22 15:49:37 +0900

    add 職種クラスに等価判定を実装

--- src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/job_apply/domain/model/job/Job.kt
+++ src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/job_apply/domain/model/job/Job.kt
@@ -6,4 +6,14 @@ class Job(val id: JobID, val name: JobName) {
     override fun toString(): String {
         return name.toString()
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        if (javaClass != other?.javaClass) return false
+
+        other as Job
+
+        return id == other.id
+    }
 }
--- src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/job_apply/domain/model/job/JobID.kt
+++ src/main/kotlin/com/tokorogadokkoi/kotlin/joboffer/job_apply/domain/model/job/JobID.kt
@@ -10,4 +10,14 @@ class JobID(val id: BigInteger) {
     override fun toString(): String {
         return id.toString()
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        if (javaClass != other?.javaClass) return false
+
+        other as JobID
+
+        return id == other.id
+    }
 }
